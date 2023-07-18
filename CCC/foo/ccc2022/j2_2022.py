32125264120018ff437e38db30250d8171048993
Justin Gong <justin.gong@ubc.ca>
2023-07-18 12:00:44 -0700

    changed answer for j2

--- CCC/foo/ccc2022/j2_2022.py
+++ CCC/foo/ccc2022/j2_2022.py
@@ -1,13 +1,14 @@
-player_list = (int(input()))*[4*[0]]
-i, p = 0, 0
-while i < len(player_list):
-    player_list[i][1] = int(input())
-    player_list[i][2] = int(input())
-    player_list[i][3] = player_list[i][1] * 5 - player_list[i][2] * 3
-    if player_list[i][3] > 40:
+n, i, p = int(input()), 0, 0
+
+while i < n:
+    a = int(input())
+    b = int(input())
+    rate = a * 5 - b * 3
+    if rate > 40:
         p += 1
     i += 1
-if p == len(player_list):
+    
+if p == n:
     print(str(p) + "+")
 else:
     print(p)
