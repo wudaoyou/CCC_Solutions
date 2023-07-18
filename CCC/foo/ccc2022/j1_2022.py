23bed3239574a6392b51cf4848a55a946afad303
Justin Gong <justin.gong@ubc.ca>
2023-07-18 15:34:49 -0700

    solution 2022

--- CCC/foo/ccc2022/j1_2022.py
+++ CCC/foo/ccc2022/j1_2022.py
@@ -2,7 +2,8 @@ r = (int(input()))
 s = (int(input()))
 
 total_num = r * 8 + s * 3
-if total_num >= 28:
-    print(total_num - 28)
-else:
-    print(0)
+if __name__ == '__main__':
+    if total_num >= 28:
+        print(total_num - 28)
+    else:
+        print(0)
