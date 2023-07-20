d367cfe49a9e2ecd07b3f03eaec8440616b56ad7
Justin Gong <justin.gong@ubc.ca>
2023-07-20 15:42:07 -0700

    changed code with j3 to get correct answers

--- CCC/foo/ccc2022/j3_2022.py
+++ CCC/foo/ccc2022/j3_2022.py
@@ -1,13 +1,22 @@
-tune, a, i = input(), "", ""
+import string
 
-for i in tune:
-    if i == "+":
-        a = a + " tighten "
-    elif i == "-":
-        a = a + " loosen "
-    elif i.isnumeric():
-        a = a + i + "\n"
+tune, l, s = input(), [], ""
+alph = list(string.ascii_uppercase)
+
+for i,j in enumerate(tune[:-1]):
+    if j not in alph and tune[i+1] in alph:
+        s += j
+        l.append(s)
+        s = ""
     else:
-        a += i
-        
-print(a)
+        s += j
+
+s += tune[-1]
+l.append(s)
+for i in l:
+    if "+" in i:
+        i = i.split("+")
+        print(i[0], "tighten", i[1])
+    elif "-" in i:
+        i = i.split("-")
+        print(i[0], "loosen", i[1])
