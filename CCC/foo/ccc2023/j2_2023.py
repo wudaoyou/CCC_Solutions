044397996c29fbdacb434a858ad3c27890f986c7
Justin Gong <justin.gong@ubc.ca>
2023-07-20 16:24:18 -0700

    finished j2

---
+++ CCC/foo/ccc2023/j2_2023.py
@@ -0,0 +1,10 @@
+# list includes pepper name and SHU
+shu_list = {"Poblano":1500, "Mirasol":6000, "Serrano":15500, "Cayenne":40000, "Thai":75000, "Habanero":125000}
+total_shu = 0
+
+# find out the total shu
+for t in range(int(input())):
+    total_shu += shu_list[input()]
+
+# print output
+print(total_shu)
